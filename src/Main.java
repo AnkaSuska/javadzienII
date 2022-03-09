8664acddb1f7f9a5cb3a9a4f925d74c0474ac8dd
AnkaSuska <100162260+AnkaSuska@users.noreply.github.com>
2022-03-09 19:20:25 +0000

    add if file

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-
         Scanner scanner = new Scanner (System.in);
-        System.out.println("Jak masz na imie? ");
+        System.out.println(" Jak masz na imie? ");
         String name = scanner.nextLine();
         System.out.println(" Jak masz na nazwisko? ");
         String surname = scanner.nextLine();
-        String age = scanner.nextLine();
+        System.out.println(" Ile masz lat? ");
+        int age = scanner.nextInt();
+    if (age>=18) {
+    System.out.println(name +" "+ surname + " Mozesz wejsc");
+}else{
+    System.out.println(name +" "+ surname + " Nie mozesz wejsc");
 
-        System.out.println("Czesc" +name +surname);
+
+}
 
 
     }
