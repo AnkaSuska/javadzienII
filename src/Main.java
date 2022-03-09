eef488233237fe3564eb7abc48e98f16ea6365b0
AnkaSuska <100162260+AnkaSuska@users.noreply.github.com>
2022-03-09 18:41:52 +0000

    add scanner file

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Jak masz na imie? ");
+        String name = scanner.nextLine();
+        System.out.println(" Jak masz na nazwisko? ");
+        String surname = scanner.nextLine();
+        String age = scanner.nextLine();
+
+        System.out.println("Czesc" +name +surname);
+
+
+    }
 }
